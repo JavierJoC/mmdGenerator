176dc6576f0a2387e2ddf4e3ca7095383f312c4d
Francisco Javier José Angeles <bocho_zic@gmail.com>
2026-01-05 18:08:03 -0600

    Se integra el identificador de la rama actual

--- mmdReder.sh
+++ mmdReder.sh
@@ -3,7 +3,7 @@
 #Autor: Francisco Javier José Angeles  bocho_zic@hotmail.com
 #Fecha de creación: dom 04 ene 2026 16:50:28 CST
 
-
+echo "$(git log --oneline -1)"
 ###     CONFIGURACIONES INICIALES   ####
 set -eu #-e termina el scrip de inmediato si algo falla   -u trata las variables no definidas como error
 export PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium
